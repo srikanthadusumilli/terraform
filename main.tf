525a3be6c2c7815fe51ffb900e16a2c468af61bf
srikanthadusumilli <48491733+srikanthadusumilli@users.noreply.github.com>
2023-05-08 14:11:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
-  access_key = "AKIARTUSW67GMUORTNEF"
-  secret_key = "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"
+  #access_key = "AKIARTUSW67GMUORTNEF"
+  #secret_key = "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"
 } 
 resource "aws_instance" "web" {
   ami           = "ami-0f8ca728008ff5af4"
